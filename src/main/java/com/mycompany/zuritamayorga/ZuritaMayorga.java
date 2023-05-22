d389ed0e554421bc8c89cd21571212ecd9996816
ZuritaKelvin <119115244+ZuritaKelvin@users.noreply.github.com>
2023-05-22 22:17:41 +0200

    Update ZuritaMayorga.java

--- src/main/java/com/mycompany/zuritamayorga/ZuritaMayorga.java
+++ src/main/java/com/mycompany/zuritamayorga/ZuritaMayorga.java
@@ -1,7 +1,7 @@
 package com.mycompany.zuritamayorga;
 
 public class ZuritaMayorga {
-
+//Kelvin Zurita Mayorga
     public static void main(String[] args) {
         Zurita z = new Zurita();
         z.saludar();
